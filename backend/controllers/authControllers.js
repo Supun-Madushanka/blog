c4b37b2841b51404c8cf0ee6ab237888d65291bf
IT22602114 <it22602114@my.sliit.lk>
2024-12-21 10:06:42 +0530

    create signin api

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel")
 const bcryptjs = require('bcryptjs')
 const erroHandler = require("../utils/error")
+const jwt = require('jsonwebtoken')
 
 const signup = async (req, res, next) => {
     const { username, email, password } =  req.body
@@ -27,6 +28,35 @@ const signup = async (req, res, next) => {
     
 }
 
+const signin = async (req, res, next) => {
+    const { email, password } = req.body
+
+    if(!email || !password || email === '' || password === ''){
+        next(erroHandler(400, 'All fields are required'))
+    }
+
+    try {
+        const validUser = await User.findOne({ email })
+        if(!validUser){
+            return next(erroHandler(404, 'User not found'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword){
+            return next(erroHandler(400, 'Invalid password'))
+        }
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+
+        const { password: pass, ...rest } = validUser._doc
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest)
+    } catch (error) {
+        next(error)
+    }
+}
+
 module.exports = {
-    signup
+    signup,
+    signin
 }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { signup,
+        signin
 
  } = require('../controllers/authControllers')
 
 const router = express.Router()
 
 router.post('/signup', signup)
+router.post('/signin', signin)
 
 module.exports = router
