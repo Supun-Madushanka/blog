0f769a0bd7910213a1c85d39503c2dd73510c89f
IT22602114 <it22602114@my.sliit.lk>
2024-12-22 12:25:53 +0530

    complete image upload

--- frontend/src/components/DashProfile.js
+++ frontend/src/components/DashProfile.js
@@ -1,19 +1,122 @@
-import { Button, TextInput } from 'flowbite-react'
-import { useSelector } from 'react-redux'
+import { Alert, Button, TextInput } from 'flowbite-react';
+import { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import axios from 'axios';
 
-const DashProfile = () => {
-  const { currentUser } = useSelector((state) => state.user)
+export default function DashProfile() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImageToCloudinary();
+    }
+  }, [imageFile]);
+
+  const uploadImageToCloudinary = async () => {
+    setImageFileUploadError(null);
+    setImageFileUploadProgress(0);
+
+    if (imageFile.size > 2 * 1024 * 1024) { 
+      setImageFileUploadError(
+        'Could not upload image. Please ensure the file size is less than 2MB and try again.'
+      );
+      setImageFileUploadProgress(null);
+      setImageFile(null);
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', imageFile);
+    formData.append('upload_preset', 'user_profile_photos'); 
+
+    try {
+      const response = await axios.post(
+        `https://api.cloudinary.com/v1_1/dxhzkog1c/image/upload`, 
+        formData,
+        {
+          onUploadProgress: (progressEvent) => {
+            const progress = Math.round(
+              (progressEvent.loaded / progressEvent.total) * 100
+            );
+            setImageFileUploadProgress(progress);
+          },
+        }
+      );
+
+      setImageFileUrl(response.data.secure_url);
+    } catch (error) {
+      setImageFileUploadError(
+        'Could not upload image. Please ensure the file size is less than 2MB and try again.'
+      );
+      setImageFileUploadProgress(null);
+      setImageFile(null);
+    }
+  };
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
+        <input
+          type='file'
+          accept='image/*'
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'
+          onClick={() => filePickerRef.current.click()}
+        >
+          {imageFileUploadProgress && (
+            <CircularProgressbar
+              value={imageFileUploadProgress || 0}
+              text={`${imageFileUploadProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: '100%',
+                  height: '100%',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${
+                    imageFileUploadProgress / 100
+                  })`,
+                },
+              }}
+            />
+          )}
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt='user'
-            className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray] ${
+              imageFileUploadProgress &&
+              imageFileUploadProgress < 100 &&
+              'opacity-60'
+            }`}
           />
         </div>
+        {imageFileUploadError && (
+          <Alert color='failure'>{imageFileUploadError}</Alert>
+        )}
         <TextInput
           type='text'
           id='username'
@@ -28,15 +131,13 @@ const DashProfile = () => {
         />
         <TextInput type='password' id='password' placeholder='password' />
         <Button type='submit' gradientDuoTone='purpleToBlue' outline>
-            Update
+          Update
         </Button>
       </form>
-      <div className="text-red-500 flex justify-between mt-5">
+      <div className='text-red-500 flex justify-between mt-5'>
         <span className='cursor-pointer'>Delete Account</span>
         <span className='cursor-pointer'>Sign Out</span>
       </div>
     </div>
-  )
+  );
 }
-
-export default DashProfile
