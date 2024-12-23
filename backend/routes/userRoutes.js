f8bf6bf8cf2c086a1ce577cadf835e58ec1a91b6
IT22602114 <it22602114@my.sliit.lk>
2024-12-23 13:00:55 +0530

    complete delete api route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { updateUser } = require('../controllers/userControllers')
+const { updateUser, deleteUser } = require('../controllers/userControllers')
 const verifyToken = require('../utils/verifyUser')
 
 const router = express.Router()
 
 router.put('/update/:userId',verifyToken, updateUser)
+router.delete('/delete/:userId', verifyToken, deleteUser)
 
 module.exports = router
