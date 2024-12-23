acc6e3dd6d2dcac9c3eec127dfe0cc1a29900077
IT22602114 <it22602114@my.sliit.lk>
2024-12-23 14:22:56 +0530

    complete sign out

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -57,7 +57,16 @@ const deleteUser = async (req, res, next) => {
     }
 }
 
+const signout = (req, res, next) => {
+    try {
+        res.clearCookie('access_token').status(200).json('User has been signed out')
+    } catch (error) {
+        next(error)
+    }
+}
+
 module.exports = {
     updateUser,
-    deleteUser
+    deleteUser,
+    signout
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { updateUser, deleteUser } = require('../controllers/userControllers')
+const { updateUser, deleteUser, signout } = require('../controllers/userControllers')
 const verifyToken = require('../utils/verifyUser')
 
 const router = express.Router()
 
 router.put('/update/:userId',verifyToken, updateUser)
 router.delete('/delete/:userId', verifyToken, deleteUser)
+router.post('/signout', signout)
 
 module.exports = router
