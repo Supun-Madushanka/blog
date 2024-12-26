559e332a61064b771c49cd301afa7eb571c2e935
IT22602114 <it22602114@my.sliit.lk>
2024-12-26 10:11:43 +0530

    complete update post

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import FooterCom from './components/FooterCom';
 import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
 
         <Route element={<OnlyAdminPrivateRoute/>}>
           <Route path='/create-post' element={<CreatePost/>}/>
+          <Route path='/update-post/:postId' element={<UpdatePost/>}/>
         </Route>
               
         <Route path='/projects' element={<Projects/>}/>
