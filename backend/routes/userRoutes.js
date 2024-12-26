a36dd3af9810ae1b5974711dc702a1b3af4599d3
IT22602114 <it22602114@my.sliit.lk>
2024-12-26 10:41:50 +0530

    create get users api route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { updateUser, deleteUser, signout } = require('../controllers/userControllers')
+const { updateUser, deleteUser, signout, getUsers } = require('../controllers/userControllers')
 const verifyToken = require('../utils/verifyUser')
 
 const router = express.Router()
@@ -7,5 +7,6 @@ const router = express.Router()
 router.put('/update/:userId',verifyToken, updateUser)
 router.delete('/delete/:userId', verifyToken, deleteUser)
 router.post('/signout', signout)
+router.get('/getUsers', verifyToken, getUsers)
 
 module.exports = router
