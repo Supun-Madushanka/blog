9a58b482e04eb117ba60797ae8f4683e13f29846
IT22602114 <it22602114@my.sliit.lk>
2024-12-26 09:32:26 +0530

    complete delete post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -69,7 +69,20 @@ const getposts = async (req, res, next) => {
     }
 }
 
+const deletepost = async (req, res, next) => {
+    if(!req.user.isAdmin || req.user.id !== req.params.userId){
+        return next(errorHandler(403, 'You are not allowed to delete this post'))
+    }
+    try {
+        await Post.findByIdAndDelete(req.params.postId)
+        res.status(200).json('The post has been deleted')
+    } catch (error) {
+        next(error)
+    }
+}
+
 module.exports = {
     create,
-    getposts
+    getposts,
+    deletepost
 }
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const verifyToken = require('../utils/verifyUser')
-const { create, getposts } = require('../controllers/postController')
+const { create, getposts, deletepost } = require('../controllers/postController')
 
 const router = express.Router()
 
 router.post('/create', verifyToken, create)
 router.get('/getposts', getposts)
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
 
 module.exports = router
